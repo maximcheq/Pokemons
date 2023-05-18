942ac825f55424d7a748dced0892b799043c1271
Max Kuklitski <mkuklitskij@gmail.com>
2023-05-18 18:20:56 +0300

    setup tableView

--- Pokemons/Pokemons/ViewController.swift
+++ Pokemons/Pokemons/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "pokemonCell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Pokemons"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.dataSource = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pokemonCell", for: indexPath)
+        cell.textLabel?.text = "test"
+        return cell
+    }
+}
 
+extension ViewController: UITableViewDelegate {
+    
 }
 
